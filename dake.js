ae6cc440cf9ec864a6d6eabb618eb578a1946e59
Liu Tuo <liu_tu@worksap.co.jp>
2016-03-03 17:47:30 +0800

    fix bug and format code

--- dake.js
+++ dake.js
@@ -11,47 +11,48 @@ if (args.length != 2) {
 }
 
 var data = querystring.stringify({
-  dakoku: 'syussya',
-  timezone: 480,
-  user_id: args[0],
-  password: args[1],
+    dakoku: 'syussya',
+    timezone: 480,
+    user_id: args[0],
+    password: args[1],
 });
 
 var options = {
-  hostname: 'ckip.worksap.co.jp',
-  port: 443,
-  path: '/cws/cws/srwtimerec',
-  method: 'POST',
-  headers: {
-    'Origin': 'https://ckip.worksap.co.jp',
-    'Host': 'ckip.worksap.co.jp',
-    'Referer': 'https://ckip.worksap.co.jp/cws/cws/srwtimerec',
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Content-Length': Buffer.byteLength(data)
-  }
+    hostname: 'ckip.worksap.co.jp',
+    port: 443,
+    path: '/cws/cws/srwtimerec',
+    method: 'POST',
+    headers: {
+        'Origin': 'https://ckip.worksap.co.jp',
+        'Host': 'ckip.worksap.co.jp',
+        'Referer': 'https://ckip.worksap.co.jp/cws/cws/srwtimerec',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': Buffer.byteLength(data)
+    }
 };
 
-var req = https.request(options, function(res) {
-  res.setEncoding('utf8');
-    if (res.statusCode == 200) {
-        console.log("打卡成功");
-    }
 
-});
-var sendRequest = function() {
+var sendRequest = function () {
+    var req = https.request(options, function (res) {
+        res.setEncoding('utf8');
+        if (res.statusCode == 200) {
+            console.log("打卡成功");
+        }
+
+    });
     req.write(data);
     req.end();
 }
 
-var getCurrentTime = function() {
+var printCurrentTime = function () {
     var now = moment()
     var formatted = now.format('YYYY-MM-DD HH:mm:ss Z')
     console.log(formatted)
 }
 
 // 0 - 10 minutes random offset
-var randomOffset = function() {
-    return Math.round(Math.random()*10*60);
+var randomOffset = function () {
+    return Math.round(Math.random() * 10 * 60);
 }
 
 var rule = new schedule.RecurrenceRule();
@@ -60,10 +61,10 @@ rule.dayOfWeek = [new schedule.Range(1, 5)];
 rule.hour = 14;
 rule.minute = 38;
 
-var schdeuledJob = schedule.scheduleJob(rule, function(){
+var schdeuledJob = schedule.scheduleJob(rule, function () {
     sleep.sleep(randomOffset())
     console.log('Today you are protected by the Dake.js');
-    getCurrentTime();
+    printCurrentTime();
     sendRequest();
 });
 console.log("Dake Job Scheduled for " + args[0] + "!!");
